e73a1995f5bbe710d73faf40312db2d7c2c8c862
Han <lenghan1991@gmail.com>
2016-08-31 11:50:09 +0800

    fix

--- themes/crystal_maiden/html/email.html.php
+++ themes/crystal_maiden/html/email.html.php
@@ -1,37 +1,36 @@
 <?php
 $view->extend(":$template:base.html.php");
 ?>
-<link rel="stylesheet" href="<?php echo $view['assets']->geturl('themes/crystal_maiden/css/crystal_maiden.css'); ?>" type="text/css" />
-<div class="cw-container">
-    <div class="cw-wrapper-container">
-        <div class="cw-logo">
+<div style="background-color: #f8f8f8; font-family: 'PingFang SC', 'Hiragino Sans GB', arial, 'microsoft yahei', sans-serif; font-size: 14px; min-width: 850px;">
+    <div style="background-color: #FFFFFF; margin: 75px auto 40px; width: 700px; min-width: 700px; height: 100%; box-shadow: 0 4px 14px 5px rgba(166, 166, 177, 0.07);">
+        <div style="padding-top: 33px; padding-left: 90px;">
             <img src="https://snow-assets.b0.upaiyun.com/assets/logo-white-2d190d988f772604c8e00fb85c632933.png" width=105 height=20 alt="cw-logo">
         </div>
-        <div class="cw-slogan">
-            <div class="cw-slogan1">
+        <div style="margin: 30px auto;">
+            <div style="margin: 0 auto; height: 40px; line-height: 40px; vertical-align: middle; font-size: 20px; text-align: center; color: #17B1F7;">
                  <?php $view['slots']->output('slogan1'); ?>
             </div>
-            <div class="cw-slogan2">
+            <div style="margin: 0 auto; height: 50px; line-height: 50px; vertical-align: middle; font-size: 30px; text-align: center; color: #666666;">
                  <?php $view['slots']->output('slogan2'); ?>
             </div>
         </div>
-        <div class="cw-banner">
+        <div style="margin-top: 40px;">
             <img src="https://mautic.b0.upaiyun.com/themes/crystal_maiden/crystal_maiden_banner.png" width=700 height=211 alt="cw-banner">
         </div>
-        <div class="cw-body">
+        <div style="color: #666666;line-height: 24px;margin: 58px 90px 40px;">
               <?php $view['slots']->output('body'); ?>
         </div>
-        <div class="cw-speical-btn">
+        <div style="background-color: #17B1F7; margin: 0 auto; width: 246px; height: 40px; margin-bottom: 40px; border-radius: 2px;">
             <?php $view['slots']->output('special-btn'); ?>
         </div>
-        <div class="cw-info">
+        <div style="margin: 0 90px;color: #666666;">
             <?php $view['slots']->output('info'); ?>
         </div>
-        <div class="cw-signature">
-            <div class="cw-username">
+        <div style="margin: 84px 90px 43px;">
+            <div style="font-size: 24px; color: #333333; margin-bottom: 14px; height: 28px;">
                 <?php $view['slots']->output('username'); ?>
             </div>
-            <div class="cw-title">
+            <div style="color: #666666; height: 18px; margin-bottom: 11px;">
                 <?php $view['slots']->output('title'); ?>
             </div>
             <div class="cw-site">
@@ -39,12 +38,12 @@ $view->extend(":$template:base.html.php");
             </div>
         </div>
     </div>
-    <div class="cw-footer">
-        <div class="cw-footer-slogan">
+    <div style="margin: 0 auto 33px; min-width: 700px; text-align: center;">
+        <div style="font-size: 24px; color: #333333; line-height: 40px; margin-bottom: 13px;">
             <?php $view['slots']->output('footer-slogan'); ?>
         </div>
-        <div class="cw-footer-sent">此邮件由100offer发送 </div>
-        <div class="cw-footer-unsubscribe"><a href="{unsubscribe_url}">取消推送邮件订阅？</a></div>
+        <div style="line-height: 17px;color: #A6A6A6;">此邮件由100offer发送 </div>
+        <div style="line-height: 17px;color: #A6A6A6;"><a style="color: #A6A6A6;text-decoration: underline;" href="{unsubscribe_url}">取消推送邮件订阅？</a></div>
     </div>
 </div>
 
--- themes/lina_inverse/html/email.html.php
+++ themes/lina_inverse/html/email.html.php
@@ -1,32 +1,31 @@
 <?php
 $view->extend(":$template:base.html.php");
 ?>
-<link rel="stylesheet" href="<?php echo $view['assets']->geturl('themes/lina_inverse/css/lina_inverse.css'); ?>" type="text/css" />
-<div class="lina-container">
-    <div class="lina-wrapper-container">
-        <div class="lina-header">
-            <img src="https://snow-assets.b0.upaiyun.com/assets/logo-white-2d190d988f772604c8e00fb85c632933.png" class="lina-header-logo" alt="lina-logo">
-            <span class="lina-header-spliter"></span>
-            <span class="lina-header-slogan">
+<div style="margin-top: 20px;background-color: #f8f8f8; font-family: 'PingFang SC', 'Hiragino Sans GB', arial, 'microsoft yahei', sans-serif; font-size: 14px; min-width: 850px;">
+    <div style="background-color: #FFFFFF; margin: 75px auto 40px; width: 700px; min-width: 700px; height: 100%; box-shadow: 0 4px 14px 5px rgba(166, 166, 177, 0.07);">
+        <div style="padding-top: 33px; padding-left: 90px; line-height: 40px;">
+            <img src="https://snow-assets.b0.upaiyun.com/assets/logo-white-2d190d988f772604c8e00fb85c632933.png" style="width: 105px; height: 20px; vertical-align: middle;" alt="lina-logo">
+            <span style="border-right: 1px solid #DCDCDC; margin: 0 19px;"></span>
+            <span style="vertical-align: middle; font-size: 16px; font-color: #666666;">
                 帮最好的程序员发现更好的offer
             </span>
         </div>
-        <div class="lina-spliter"></div>
-        <div class="lina-body">
+        <div style="margin: 40px 90px; border-top: 1px solid #F6F6F6;"></div>
+        <div style="color: #666666; line-height: 24px; margin: 58px 90px 40px;">
               <?php $view['slots']->output('body'); ?>
         </div>
-        <div class="lina-speical-btn">
+        <div style="background-color: #17B1F7; margin-left: 90px; width: 246px; height: 40px; margin-bottom: 40px; border-radius: 2px;">
             <?php $view['slots']->output('special-btn'); ?>
         </div>
-        <div class="lina-info">
+        <div style="margin: 0 90px; color: #666666;">
             <?php $view['slots']->output('info'); ?>
         </div>
-        <div class="lina-spliter"></div>
-        <div class="lina-signature">
-            <div class="lina-username">
+        <div style="margin: 40px 90px; border-top: 1px solid #F6F6F6;"></div>
+        <div style="margin: 0 90px 43px;">
+            <div style="font-size: 24px; color: #333333; margin-bottom: 14px; height: 28px;">
                 <?php $view['slots']->output('username'); ?>
             </div>
-            <div class="lina-title">
+            <div style="color: #666666; height: 18px; margin-bottom: 11px;">
                 <?php $view['slots']->output('title'); ?>
             </div>
             <div class="lina-site">
@@ -34,9 +33,9 @@ $view->extend(":$template:base.html.php");
             </div>
         </div>
     </div>
-    <div class="lina-footer">
-        <div class="lina-footer-sent">此邮件由100offer发送 </div>
-        <div class="lina-footer-unsubscribe"><a href="{unsubscribe_url}">取消推送邮件订阅？</a></div>
+    <div style="margin: 0 auto 33px; min-width: 700px; text-align: center;">
+        <div style="line-height: 17px; color: #A6A6A6;">此邮件由100offer发送 </div>
+        <div style="line-height: 17px; color: #A6A6A6;"><a style="color: #A6A6A6; text-decoration: underline;" href="{unsubscribe_url}">取消推送邮件订阅？</a></div>
     </div>
 </div>
 
